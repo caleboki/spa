9f113a9713ceb94ff235ac21c3502abcbfd002d9
Caleb Oki <caleboki@gmail.com>
2016-04-17 22:33:25 +0100

    More progress on Blog

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,19 +2,42 @@
 
 @section('title') All Posts @endsection
 
-
 @section('content')
 
-<div class="row">
-	<div class="col-md-9">
-		<h1 style="margin-left: 150px;">All Posts</h1>
-	</div>
+<div class="col-lg-10 col-lg-offset-1">
 
-	<div class="col-md-2">
-		<a href="{{ route('posts.create') }}" class="btn btn-lg btn-block btn-primary" style="margin-top: 18px;">Create New Post </a>
+    <h1><i class="fa fa-book"></i> All Posts <a href="{{ route('posts.create') }}" class="btn btn-primary pull-right">Create New Post</a></h1>
+    <hr>
+    <br>
+
+    <div class="table-responsive">
+        <table class="table table-bordered table-striped">
+
+            <thead>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                    <th>Date/Time Added</th>
+                    <th>Operations</th>
+            </thead>
+
+            <tbody>
+            @foreach ($posts as $post)
+  					<tr>
+  						<th>{{ $post->id }}</th>
+  						<td>{{$post->title }}</td>
+  						<td>{{ substr($post->body, 0, 50) }}{{ strlen($post->body) > 50 ? "...":""}}</td>
+  						<td>{{$post->created_at->format('F d, Y h:ia') }}</td>
+  						<td><a href="{{ route('posts.show', $post->id) }}" class="btn btn-default btn-sm">View</a> <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a></td>
+  					</tr>
+
+  			@endforeach
+
+  			
+  				
+  			</tbody>
+		</table>
+		
 	</div>
-	<hr>
-	
-</div>
 
 @endsection
